8649f6375a66163c2cfc7b91d59e7537ff240590
David Yoon <qqpo12@naver.com>
2023-01-04 19:59:22 +0900

    Add a ClassLCComponent.jsx
    * Added new compoenet to explain Class component LifeCycle

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Composition from './components/2-4.Props/Composition';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponent from './components/2-5.State/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import ClassLCComponent from './components/2-6.LifeCycle/ClassLCComponent';
 
 function App() {
   return (
     <div className="App">
+      <ClassLCComponent />
       <FunctionalComponent />
       <ClassComponent />
       <Composition />
