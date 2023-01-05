5d737d268c6ac1da8d3497e7645698add52ab950
David Yoon <qqpo12@naver.com>
2023-01-05 21:04:54 +0900

    Add a Event.jsx
    * Added a Event React Component to handle button event

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponent from './components/2-5.State/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 import ClassLCComponent from './components/2-6.LifeCycle/ClassLCComponent';
+import Event from './components/2-7.Event/Event';
 
 function App() {
   return (
     <div className="App">
-      <ClassLCComponent />
+      <Event />
+      {/* <ClassLCComponent />
       <FunctionalComponent />
       <ClassComponent />
       <Composition />
-      <Extraction />
+      <Extraction /> */}
     </div>
   );
 }
