9c89581f4eccbcbeb86b65109fe1a79cb0c11e69
David Yoon <qqpo12@naver.com>
2023-01-17 22:32:13 +0900

    Add Condition component for the ConditionalRending
    * Added a Condition.jsx file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import Composition from './components/2-4.Props/Composition';
-import Extraction from './components/2-4.Props/Extraction/Extraction';
-import ClassComponent from './components/2-5.State/ClassComponent';
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
-import ClassLCComponent from './components/2-6.LifeCycle/ClassLCComponent';
-import Event from './components/2-7.Event/Event';
+import Condition from './components/2-8.ConditionalRendering/Condition';
 
 function App() {
   return (
     <div className="App">
-      <Event />
-      {/* <ClassLCComponent />
-      <FunctionalComponent />
-      <ClassComponent />
-      <Composition />
-      <Extraction /> */}
+      <Condition />
     </div>
   );
 }
